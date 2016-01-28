e3c14b2dcab5efcf3de14deb74601fb281aac3e1
Ardian Ajvazi <ardian_46@hotmail.com>
2016-01-28 14:48:00 -0800

    able to create user

--- ardian/server.js
+++ ardian/server.js
@@ -6,8 +6,10 @@ var port = 3000;
 
 const sharksRouter = require(__dirname + '/routes/sharks_routes');
 const peopleRouter = require(__dirname + '/routes/people_routes');
+const authRouter = require(__dirname + '/routes/auth_routes');
 
 app.use('/api', sharksRouter);
 app.use('/api', peopleRouter);
+app.use('/api', authRouter);
 
 module.exports.server = app.listen(port, () => console.log('Server running ' + port));//eslint-disable-line
